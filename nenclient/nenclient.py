37d6a59165618c58145507a208e42338d852aeda
David Schwarz <dsdavidschwarz@gmail.com>
2019-10-27 21:30:39 -0500

    adds basic socket test

--- nenclient/nenclient.py
+++ nenclient/nenclient.py
@@ -1,1 +1,25 @@
-# nenclient main file #
+#
+#   Hello World client in Python
+#   Connects REQ socket to tcp://localhost:5555
+#   Sends "Hello" to server, expects "World" back
+#
+
+import zmq
+
+print("test")
+
+context = zmq.Context()
+
+#  Socket to talk to server
+print("Connecting to hello world server…")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5555")
+
+#  Do 10 requests, waiting each time for a response
+while (1):
+    print("Sending request …")
+    socket.send(b"Hello")
+
+    #  Get the reply.
+    message = socket.recv()
+    print("Received reply [ %s ]" % (message))
